77e667e36415e112eb151ed749923fe6daf4b201
lei <leimeituan@163.com>
2021-05-09 17:07:34 +0800

    02 reference.cpp

---
+++ 02reference/reference.cpp
@@ -0,0 +1,30 @@
+//
+// Created by xialei07 on 5/9/21.
+//
+
+#include "reference.h"
+#include <iostream>
+#define LOG(x) std::cout << x << std::endl;
+
+void Increment(int& value) {
+    value++;
+}
+
+int main() {
+
+    int a = 5;
+    int b = 8;
+
+    int* ref = &a;
+    *ref = 2;
+    ref = &b;
+    *ref = 1;
+
+    LOG(a);
+    LOG(b);
+
+    Increment(a);
+    //LOG(a);
+
+    std::cin.get();
+}
---
+++ 02reference/reference.h
@@ -0,0 +1,14 @@
+//
+// Created by xialei07 on 5/9/21.
+//
+
+#ifndef HELLOWORLD_REFERENCE_H
+#define HELLOWORLD_REFERENCE_H
+
+
+class reference {
+
+};
+
+
+#endif //HELLOWORLD_REFERENCE_H
