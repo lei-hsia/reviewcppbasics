e688db05eb3a5f99b17ff482e2aecec1b2641af9
lei <leimeituan@163.com>
2021-05-23 17:00:14 +0800

    reserve, emplace_back

--- 12STLexamples/stldemo.cpp
+++ 12STLexamples/stldemo.cpp
@@ -8,6 +8,11 @@
 
 struct Vertex {
     float x, y, z;
+    Vertex(float x, float y, float z) : x(x), y(y), z(z) {}
+
+    Vertex(const Vertex& vertex) : x(vertex.x), y(vertex.y), z(vertex.z) {
+        std::cout << "Copied:" << x << ", " << y << ", " << z << ", " << std::endl;
+    }
 };
 
 // overloading operator to stream templates: write things to stream
@@ -16,16 +21,12 @@ std::ostream& operator<<(std::ostream& ostream, const Vertex& vertex){
     return ostream;
 }
 
-void function(const std::vector<Vertex>& vertices){
-
-}
-
 int main() {
     std::vector<Vertex> vertices;
-    vertices.push_back({1,2,3});
-    vertices.push_back({11,22,33});
-
-    vertices.erase(vertices.begin()+1);
+    vertices.reserve(3);
+    vertices.emplace_back(1,2,3);
+    vertices.emplace_back(4,5,6);
+    vertices.emplace_back(7,8,9);
 
     for (const Vertex& v : vertices) {
         std::cout << v << std::endl;
