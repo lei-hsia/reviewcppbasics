d5e9bb138020f1d32ea96973e42ce0991ea11d1d
lei <leimeituan@163.com>
2021-05-29 18:25:24 +0800

    lambda function usage

--- 15funcptr/functionptrbetter.cpp
+++ 15funcptr/functionptrbetter.cpp
@@ -5,13 +5,16 @@
 #include "functionptrbetter.h"
 #include <iostream>
 #include <vector>
+#include <functional>
 
-void foreach(const std::vector<int>& nums, void(*func)(int)){
+void foreach(const std::vector<int>& nums, const std::function<void(int)>& func){
     for (int num: nums) func(num);
 }
 
 int main() {
     std::vector<int> nums = {1, 5, 4, 2, 3};
-    foreach(nums, [](int v){std::cout << "Value: "<< v << std::endl;});
+    int a = 100;
+    auto lambda = [&a](int v){std::cout << "Value: "<< v << a << std::endl;};
+    foreach(nums, lambda);
     std::cin.get();
 }
