bf2f865f9fcba820e7204110849c191880584fbf
lei <leimeituan@163.com>
2021-05-30 18:41:33 +0800

    print thread id

--- 16thread/threaddemo.cpp
+++ 16thread/threaddemo.cpp
@@ -10,6 +10,7 @@
 bool s_finished = false;
 
 void doWork() {
+    std::cout << "Started thread id= " << std::this_thread::get_id() << std::endl;
     while (!s_finished){
         std::cout << "Working ... \n";
         std::this_thread::sleep_for(std::chrono::milliseconds(1000));
@@ -25,5 +26,7 @@ int main() {
     worker.join();
 
     std::cout << "Finished." << std::endl;
+    std::cout << "Started thread id= " << std::this_thread::get_id() << std::endl;
+
     std::cin.get();
 }
